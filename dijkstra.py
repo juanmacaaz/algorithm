59a623319e8de347dbb0b6209dcf4bbed6e437a9
Juanma <juanma.caaz@gmail.com>
2020-04-09 00:37:00 +0200

    Ahora te dice el recorrido y output mas limpio, tambien coge los datos desde un fichero

--- dijkstra.py
+++ dijkstra.py
@@ -1,31 +1,32 @@
 # Este codigo es util para realizar el algoritmo de Dijksta
 
+data = open("datos.txt", "r")
+data_lines = data.readlines()
 
+list_vars = list(range(0, int(data_lines[0])))
+inicio  = data_lines[1].split(",")[0].lower()
+final   = data_lines[1].split(",")[1][0].lower()
 
-list_vars = [0,1,2,3,4,5,6,7,8,9]
+datos = []
 
-datos = [
-    [0,-1,-1,12,-1,-1,-1,-1,-1,-1],
-    [7,0,-1,-1,17,-1,-1,2,16,-1],
-    [13,4,0,-1,11,-1,7,3,-1,6],
-    [-1,18,14,0,-1,10,3,-1,15,-1],
-    [-1,5,-1,-1,0,-1,16,6,-1,-1],
-    [14,-1,19,-1,16,0,-1,-1,11,-1],
-    [-1,-1,-1,10,-1,-1,0,-1,3,12],
-    [-1,-1,-1,3,-1,-1,-1,0,-1,-1],
-    [-1,-1,-1,-1,-1,-1,14,-1,0,2],
-    [-1,-1,4,-1,-1,16,-1,-1,10,0]
-]
+for x in range(2, len(list_vars)+2):
+    line = data_lines[x]
+    nums = line.split(",")
+    sub_nums = []
+    for y in nums:
+        sub_nums.append(int(y))
+    datos.append(sub_nums)
 
-def nuevo_valores(lista_matrix, linea_valores, valor_suma, valores_cogidos):
+def nuevo_valores(lista_matrix, linea_valores, valor_suma, valores_cogidos, recorrido, siguiente_valor):
     for x in range(0, len(lista_matrix)):
         if lista_matrix[x] != -1 and x not in valores_cogidos:
             if lista_matrix[x]+valor_suma < linea_valores[x] or linea_valores[x] == -1:
                 linea_valores[x] = lista_matrix[x]+valor_suma
+                recorrido[x] = siguiente_valor
 
 
 def coger_valor_minimo(lista, valores_cogidos):
-    valor_minimo = 999999999
+    valor_minimo = 9999999999
     indice = 0
     pos_minimo = 0
     for x in lista:
@@ -36,10 +37,10 @@ def coger_valor_minimo(lista, valores_cogidos):
         indice+=1
     return valor_minimo, pos_minimo
 
-def muestra_paso(letra ,linea_valore):
+def muestra_paso(letra ,linea_valore, recorrido):
     print(chr(letra+97), end=" -- ")
     for x in linea_valore:
-        print(x, end=" ")
+        print(str(x), end=" ")
     print()
 
 def init(list_vars, datos):
@@ -50,25 +51,37 @@ def init(list_vars, datos):
         index += 1
     return matrix
 
-def mostrar_solucion(matrix, inicial):
+def mostrar_solucion(matrix, inicial, final):
     valores_cogidos = []
     linea_valores = []
     siguiente_valor = inicial
     valor_anterior_suma = 0
+    recorrido = []
+    recorrido_final = []
+    for x in range(0, len(matrix[siguiente_valor])):
+        recorrido.append(inicial)
 
     valores_cogidos.append(inicial)
     linea_valores = matrix[siguiente_valor]
 
     for x in range(0, len(matrix)):
-        muestra_paso(siguiente_valor,linea_valores)
+        recorrido_final.append(recorrido[siguiente_valor])
+        muestra_paso(siguiente_valor,linea_valores,recorrido)
         valor_anterior_suma, siguiente_valor = coger_valor_minimo(linea_valores, valores_cogidos)
         valores_cogidos.append(siguiente_valor)
-        nuevo_valores(matrix[siguiente_valor], linea_valores, valor_anterior_suma, valores_cogidos)
+        nuevo_valores(matrix[siguiente_valor], linea_valores, valor_anterior_suma, valores_cogidos, recorrido, siguiente_valor)
+        if siguiente_valor == final:
+            print("El peso es de: ", linea_valores[final])
+    recorrido_final.append(recorrido[siguiente_valor])
+    recorrido_final.append(final)
     
+    print("El recorrido es: ", end="")
+
     for x in valores_cogidos:
-        print(chr(x+97), end=" ")
+        if x in recorrido_final:
+            print(chr(x+97), end=" ")
 
 matrix = init(list_vars, datos)
 
 print("La solucion de la tabla es:")
-mostrar_solucion(matrix, ord('h')-97)
+mostrar_solucion(matrix, ord(inicio)-97, ord(final)-97)
